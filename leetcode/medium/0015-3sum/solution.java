faeeb314b5fcdcf3db6c74222cfe9e13be7eb4e7
Sampriti Biswas <sampritibiswas8@gmail.com>
2026-07-06 01:53:35 +0530

    ✅ 3Sum | Medium | Java | Runtime: 34 ms (61.22%) | Memory: 59.4 MB | 2026-07-05

---
+++ leetcode/medium/0015-3sum/solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] arr) {
+        int n = arr.length;
+        Arrays.sort(arr);
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int i = 0;i<n;i++) {
+            if(i>0 && arr[i]==arr[i-1]) continue;
+
+            int left = i+1; int right= n-1;
+
+            while(left<right) {
+                int sum = arr[i]+arr[left]+arr[right];
+                if(sum==0) {
+                    ans.add(Arrays.asList(arr[i],arr[left],arr[right]));
+                    left++;
+                    right--;
+                    while(left<right && arr[left]==arr[left-1]) left++;
+                    while(left<right && arr[right]==arr[right+1]) right--;
+                }
+                else if (sum<0) left++;
+                else right--;
+                }
+            }
+            return ans;
+        }
+    }
