7bce2006e2e30c6f3eae7b180d3e61c0eebb026d
Sampriti Biswas <sampritibiswas8@gmail.com>
2026-07-15 20:26:36 +0530

    ✅ STRINGSHIFT | Medium | Java | Runtime: N/A | Memory: N/A | 2026-07-15

---
+++ codechef/medium/STRINGSHIFT/solution.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner (System.in);
+		int t = sc.nextInt();
+		while(t-->0){
+		    int n = sc.nextInt();
+		    char[]s = sc.next().toCharArray();
+		    
+		    int i =0;
+		    
+		    while(i<n && s[i] !='z'){
+		        i++;
+		    }
+		    while(i<n && s[i]=='z'){
+		        s[i]='a';
+		        i++;
+		    }
+		    System.out.println(new String(s));
+		}
+
+	}
+}
