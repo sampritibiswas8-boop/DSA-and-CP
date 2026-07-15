05418edc4739e8ba4db3b4e9b102e30973885929
Sampriti Biswas <sampritibiswas8@gmail.com>
2026-07-15 20:15:48 +0530

    ✅ CONVEYOR | Medium | Java | Runtime: N/A | Memory: N/A | 2026-07-15

---
+++ codechef/medium/CONVEYOR/solution.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t= sc.nextInt();
+		while(t-->0){
+		    int n =sc.nextInt();
+		    int p = sc.nextInt();
+		    String s = sc.next();
+		    
+		    int l = 0;
+		    for(int i = p-1;i>=0;i++){
+		        if(s.charAt(i)=='R')
+		        l++;
+		    }
+		    int r =0;
+		    for(int i = p-1;i<n;i++){
+		        if(s.charAt(i)=='L')
+		        r++;
+		    }
+		    System.out.println(Math.min(l,r));
+		}
+
+	}
+}
