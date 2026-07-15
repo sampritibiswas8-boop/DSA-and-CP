b099d92d619df9f362e754dba2d2f1ebfe6b5a35
Sampriti Biswas <sampritibiswas8@gmail.com>
2026-07-15 20:06:28 +0530

    ✅ SQPAPER | Medium | Java | Runtime: N/A | Memory: N/A | 2026-07-15

--- codechef/medium/SQPAPER/solution.java
+++ codechef/medium/SQPAPER/solution.java
@@ -7,6 +7,12 @@ class Codechef
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		int s= Math.min(a,b);
+		System.out.println(s*s);
 
 	}
 }
