f7f256cf368e0a717a429c9639658043317dadfb
Sampriti Biswas <sampritibiswas8@gmail.com>
2026-07-21 21:40:02 +0530

    ✅ Set Matrix Zeroes | Medium | Java | Runtime: 1 ms (98.35%) | Memory: 47.7 MB | 2026-07-21

--- leetcode/medium/0073-set-matrix-zeroes/solution.java
+++ leetcode/medium/0073-set-matrix-zeroes/solution.java
@@ -1,32 +1,30 @@
 class Solution {
-   
     public void setZeroes(int[][] matrix) {
-       
+
         int m = matrix.length;
         int n = matrix[0].length;
 
-        
-        boolean firstRowZero = false;
-        
-        boolean firstColZero = false;
+        boolean firstrowzero = false;
+        boolean firstcolzero = false;
 
-       
-        for (int j = 0; j < n; j++) {
-            if (matrix[0][j] == 0) {
-                firstRowZero = true;
-                break;
+        
+            for (int j = 0; j < n; j++) {
+                if (matrix[0][j] == 0) {
+                    firstrowzero = true;
+                    break;
+                }
             }
-        }
-
-       
-        for (int i = 0; i < m; i++) {
-            if (matrix[i][0] == 0) {
-                firstColZero = true;
-                break;
-            }
-        }
+        
 
         
+            for (int i = 0; i < m; i++) {
+                if (matrix[i][0] == 0) {
+                    firstcolzero = true;
+                    break;
+                }
+            }
+        
+
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
                 if (matrix[i][j] == 0) {
@@ -36,27 +34,25 @@ class Solution {
             }
         }
 
-       
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
-                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                if ((matrix[i][0] == 0) || (matrix[0][j] == 0)) {
                     matrix[i][j] = 0;
                 }
             }
         }
 
-     
-        if (firstRowZero) {
+        if (firstrowzero) {
             for (int j = 0; j < n; j++) {
                 matrix[0][j] = 0;
             }
         }
 
-      
-        if (firstColZero) {
+        if (firstcolzero) {
             for (int i = 0; i < m; i++) {
                 matrix[i][0] = 0;
             }
         }
+
     }
 }
