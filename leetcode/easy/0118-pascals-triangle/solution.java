a8c5cd712966b06191e27ee4518ba6799ab19e64
Sampriti Biswas <sampritibiswas8@gmail.com>
2026-07-21 23:19:01 +0530

    ✅ Pascal's Triangle | Easy | Java | Runtime: 1 ms (94.28%) | Memory: 43.5 MB | 2026-07-21

--- leetcode/easy/0118-pascals-triangle/solution.java
+++ leetcode/easy/0118-pascals-triangle/solution.java
@@ -1,19 +1,28 @@
 class Solution {
+
+    public long findPascalElement(int r, int c) {
+        int n = r - 1;
+        int k = c - 1;
+
+        long result = 1;
+
+        for (int i = 0; i < k; i++) {
+            result = result * (n - i) / (i + 1);
+        }
+
+        return result;
+    }
+
     public List<List<Integer>> generate(int numRows) {
 
         List<List<Integer>> ans = new ArrayList<>();
 
-        for (int i = 0; i < numRows; i++) {
+        for (int r = 1; r <= numRows; r++) {
 
             List<Integer> row = new ArrayList<>();
 
-            long value = 1;
-
-            for (int j = 0; j <= i; j++) {
-
-                row.add((int) value);
-
-                value = value * (i - j) / (j + 1);
+            for (int c = 1; c <= r; c++) {
+                row.add((int) findPascalElement(r, c));
             }
 
             ans.add(row);
